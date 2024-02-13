06114d6d763e2b099f5cbae5103c6dba3ac5bbfe
RoshBJr <roshanjr3@gmail.com>
2024-02-13 15:30:22 -0500

    static et dynamic rendering

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -6,11 +6,13 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const response = await fetch('https://jsonplaceholder.typicode.com/users',
+  {cache: 'no-store'})
   const users:User[] = await response.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {
           users.map(user => (
