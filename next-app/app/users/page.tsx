a02ede4669e37900fe2f7edfacfc11ca44f8bb0c
RoshBJr <roshanjr3@gmail.com>
2024-02-13 16:30:59 -0500

    vidéo terminé, utilisation du daisyui routing, ssr etc

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface User {
   id: number;
   name: string;
+  email:string;
 }
 
 const UsersPage = async () => {
@@ -10,19 +11,27 @@ const UsersPage = async () => {
   {cache: 'no-store'})
   const users:User[] = await response.json();
   return (
-    <>
+    <div data-theme='dark' className='rounded-md p-2'>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {
-          users.map(user => (
-            <li key={user.id}>
-              {user.name}
-            </li>
-          ) )
-        }
-      </ul>
-    </>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+              </tr>
+            ) )
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
