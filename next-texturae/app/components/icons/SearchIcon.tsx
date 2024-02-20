0d6e40187879bf0d130b7ec077fcbc2967c83e27
RoshBJr <roshanjr3@gmail.com>
2024-02-20 14:50:48 -0500

    Header de base terminé

--- next-texturae/app/components/icons/SearchIcon.tsx
+++ next-texturae/app/components/icons/SearchIcon.tsx
@@ -1,49 +1,48 @@
 function SearchIcon() {
   return (
-    <>
+    <div className="h-[40px] max-w-60 w-60 relative">
       <input type="checkbox" id="searchInput" />
-      <label className="_ctn btn btn-ghost btn-circle w-auto" htmlFor="searchInput">
-        <div className="h-auto w-full relative">
-          {/* search icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="_search-icon h-5 w-5 checked:h-0 checked:w-0"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-          {/* cross icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="_cross-icon w-0 h-0 checked:w-5 checked:h-5 checked:absolute checked:top-1/2
-              checked:-translate-y-1/2 checked:right-0 checked:mr-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
-          </svg>
-
-          <input
-            type="text"
-            className="_search opacity-0 absolute top-0 bottom-0 right-0 left-0 
-        outline-none rounded-[25px] caret-platinum pl-2 border-thistle border text-platinum"
+      <label
+        className="_ctn z-10 absolute right-0 inline-flex justify-center items-center  w-[40px] h-[40px] btn-ghost rounded-full"
+        htmlFor="searchInput"
+      >
+        {/* search icon */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="_search-icon h-[20px] w-[20px]"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           />
-        </div>
+        </svg>
+        {/* cross icon */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="_cross-icon hidden"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
       </label>
-    </>
+      <input
+        type="text"
+        className="_search opacity-0 pr-[40px] absolute left-0 top-1/2 h-[40px] -translate-y-1/2 
+        outline-none right-0 rounded-[25px] caret-platinum pl-2 border-thistle border text-platinum"
+      />
+    </div>
   );
 }
 
--- next-texturae/app/page.tsx
+++ next-texturae/app/page.tsx
@@ -1,32 +1,28 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import {client} from '../code/sanityClient';
 import { product } from "@/code/types";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-
 export default async function Home() {
-    const data:any = await client.fetch(`*[_type == "product"] {
-      _id,
-      name,
-      category,
-      colors,
-      description,
-      gender,
-      slug_en,
-      slug_fr,
-      defaultImg,
-      image[]{ "imgUrl": asset->url }
-    }`);
+    // const data:any = await client.fetch(`*[_type == "product"] {
+    //   _id,
+    //   name,
+    //   category,
+    //   colors,
+    //   description,
+    //   gender,
+    //   slug_en,
+    //   slug_fr,
+    //   defaultImg,
+    //   image[]{ "imgUrl": asset->url }
+    // }`);
 
     // console.log(data.map(prod => prod.name.en));
 
   return (
     <main className="pt-[80px] bg-platinum">
       <section className="p-5 grid min-[320px]:grid-cols-1 min-[500px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {
+        {/* {
           data.map((product:product) => {
             
             return(
@@ -51,7 +47,7 @@ export default async function Home() {
               </Link>
             )
           })
-        }
+        } */}
       </section>
     </main>
   );
