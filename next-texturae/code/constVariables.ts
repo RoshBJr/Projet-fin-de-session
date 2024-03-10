135f0f7de96c19af692661aaed56d11f0ff33bc7
RoshBJr <roshanjr3@gmail.com>
2024-03-10 13:27:13 -0400

    build 3

---
+++ next-texturae/code/constVariables.ts
@@ -0,0 +1,3 @@
+export const TOKEN = 'skrLyfFWB0goAtATbaJ0evcqCuv4LhvSakjvVSbSZ9Ng7x5R3yzZYlPVVXdsv2sft4Do7myqjDDReRdCPurTEyHYMGD70goXQ5tOZIHevNFmKEplDsKRgcUD1LjCvLJLuvX5JjpIiVav1517bHRhQ0zP4mwvZrvY6OMI3Tih1iNdsia8aDfo';
+export const PROJECT_ID = '1aldes3z';
+export const DATASET = 'production';
