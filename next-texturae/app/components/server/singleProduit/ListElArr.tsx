be292d2de027e337eb3ec355a050317977f44ab4
RoshBJr <roshanjr3@gmail.com>
2024-03-10 15:09:39 -0400

    single produit responsive

--- next-texturae/app/components/server/singleProduit/ListElArr.tsx
+++ next-texturae/app/components/server/singleProduit/ListElArr.tsx
@@ -15,8 +15,8 @@ function ListElArr({ data,searchKey, title, selectValue,postQuery}: Props) {
 
   return data ? (
     <div className="flex flex-col gap-4">
-      <h2 className="font-font-titre text-4xl">{title}</h2>
-      <div className="flex gap-3 ml-2">
+      <h2 className="font-font-titre min-[320px]:text-3xl md:text-4xl">{title}</h2>
+      <div className="flex gap-3 ml-2 flex-wrap">
         {data.map((val, index) => (
           <Link
               key={val}
@@ -24,10 +24,11 @@ function ListElArr({ data,searchKey, title, selectValue,postQuery}: Props) {
                 `?${searchKey}=${index}${postQuery}`
 
               }
+              scroll={false}
               replace
             className={`active:border-davys-gray
               active:bg-thistle active:text-alice-blue hover:border-davys-gray hover:bg-thistle
-              hover:text-alice-blue  cursor-pointer border-2 py-2 px-4 rounded-[8px] font-font-titre text-xl
+              hover:text-alice-blue  cursor-pointer border-2 py-2 px-4 rounded-[8px] font-font-titre min-[320px]:text-sm md:text-xl
               ${index == selectVal ? "border-davys-gray bg-thistle text-alice-blue": "border-thistle bg-alice-blue text-davys-gray"} duration-200`} >
               {val}
           </Link>
--- next-texturae/app/produit/[id]/page.tsx
+++ next-texturae/app/produit/[id]/page.tsx
@@ -38,28 +38,28 @@ async function page({
   }`,undefined, {cache: 'force-cache'});
 
   return data ? (
-    <section className="_single bg-alice-blue flex justify-start gap-28 w-full mt-[80px] overflow-hidden max-h-[calc(100vh-80px)]">
+    <section className="min-[320px]:flex-col xl:flex-row _single bg-alice-blue flex justify-start xl:gap-28 w-full mt-[80px] xl:overflow-hidden min-[320px]:h-auto xl:max-h-[calc(100vh-80px)]">
       <img
-        className="w-auto h-[calc(100vh-80px)]"
+        className="object-top object-cover min-[320px]:w-full min-[320px]:h-[70vh] xl:w-auto xl:h-[calc(100vh-80px)]"
         src={data[0].defaultImg}
         alt={en ? data[0].name.en : data[0].name.fr}
       />
-      <div className="pt-16 pb-8 flex flex-col gap-8 overflow-y-scroll flex-1">
-        <h2 className="font-font-titre text-6xl text-davys-gray max-w-[90%]">
+      <div className="pt-16 pb-8 flex flex-col gap-8 overflow-y-scroll flex-1 min-[320px]:px-3 md:px-8">
+        <h2 className="font-font-titre min-[320px]:text-3xl md:text-5xl xl:text-6xl text-davys-gray xl:max-w-[90%]">
           {en ? data[0].name.en : data[0].name.fr}
         </h2>
         <div className="flex gap-3">
-          <div className="border rounded-full py-1 px-4 border-thistle font-font-titre text-2xl text-davys-gray">
+          <div className="border rounded-full py-1 px-4 border-thistle font-font-titre min-[320px]:text-xl md:text-2xl text-davys-gray">
             {en ? data[0].gender.en : data[0].gender.fr}
           </div>
-          <div className="border rounded-full py-1 px-4 border-thistle font-font-titre text-2xl text-davys-gray">
+          <div className="border rounded-full py-1 px-4 border-thistle font-font-titre min-[320px]:text-xl md:text-2xl text-davys-gray">
             {en ? data[0].category.en : data[0].category.fr}
           </div>
         </div>
-        <div className="font-font-titre text-5xl text-davys-gray">
+        <div className="font-font-titre min-[320px]:text-3xl md:text-4xl lg:text-5xl text-davys-gray">
           ${data[0].price}
         </div>
-        <p className="font-font-text text-davys-gray text-2xl w-[90%]">
+        <p className="font-font-text text-davys-gray min-[320px]:text-xl md:text-2xl xl:w-[90%]">
           {en ? data[0].description.en : data[0].description.fr}
         </p>
         <ListElArr
@@ -94,10 +94,10 @@ async function page({
         {/* <ListElObj data={data[0].fits.fr} title="Formes"/> */}
         {/* <ListElObj data={data[0].collars.fr} title="Cols"/> */}
       </div>
-      <form action={addToCart}>
+      <form className="min-[320px]:px-8 xl:px-0 min-[320px]:mb-16 xl-my-0" action={addToCart}>
         <button
           type="submit"
-          className=" cursor-pointer fixed bottom-0 right-0 mr-12 mb-11 border-davys-gray border-2 py-4 px-3 rounded-[8px] bg-thistle font-font-titre text-xl text-alice-blue duration-200 hover:bg-davys-gray active:scale-105"
+          className=" cursor-pointer xl:fixed xl:bottom-0 xl:right-0 xl:mr-12 xl:mb-11 border-davys-gray border-2 py-4 px-3 rounded-[8px] bg-thistle font-font-titre text-xl text-alice-blue duration-200 hover:bg-davys-gray active:scale-105 min-[320px]:w-full xl:w-auto"
         >
           {en ? "Add to cart" : "Ajouter au panier"}
         </button>
