bb70c2cfe8be36853bc002464d9fef60db551d6a
RoshBJr <roshanjr3@gmail.com>
2024-03-14 15:19:47 -0400

    login problem

--- next-texturae/code/actions.ts
+++ next-texturae/code/actions.ts
@@ -43,10 +43,9 @@ export async function login(formData: FormData) {
   let userName = null;
   // Verify credentials && get the user
   const cookieStore = cookies();
-  const user = {
+  let user = {
     email: formData.get("email")?.toString(),
     pass: formData.get("password")?.toString(),
-    username: formData.get("username")?.toString()
   };
   const userSanity = await client.fetch(`
       *[_type == "users"] {
@@ -60,54 +59,27 @@ export async function login(formData: FormData) {
     if (user) {
       userSanity.map((singU: any) => {
         if (singU.email == user.email) {
-          return (sanityPass = singU.pass), (cart = singU.cart), (userName = singU.username);
+          return (
+            (sanityPass = singU.pass),
+            (cart = singU.cart),
+            (userName = singU.username)
+          );
         }
       });
     }
   }
 
-  if (sanityPass) {
+  if (sanityPass && userName) {
     sanityPass = await decrypt(sanityPass);
     if (sanityPass == user.pass) {
-      if (cart && cart !== "[]") {
-        let currCart = cookieStore.get("cart")?.value;
-        if (currCart && currCart == "[]") {
-          cookieStore.set("cart", cart);
-          if(userName) cookieStore.set("user", userName)
-          const expires = new Date(Date.now() + 60 * 60 * 24 * 1000);
-          const session = await encrypt({ user, expires });
-          // Save the session in a cookie
-          return cookieStore.set("session", session, { expires, httpOnly: true });
-        }
-        if (currCart) {
-          cart = JSON.parse(cart);
-          let ids: any = [];
-          cart.map((item: any, i: number) => (ids[i] = item.id));
-          JSON.parse(currCart).map((currItem: any, i: number) => {
-            if (ids.includes(currItem.id)) {
-              cart[ids.indexOf(currItem.id)].quantity += currItem.quantity;
-            } else {
-              cart.push(currItem);
-            }
-          });
-          cart = JSON.stringify(cart);
-        }
-        cookieStore.set("cart", cart);
-        if(userName) cookieStore.set("user", userName)
-      }
+      revalidatePath('/');
       // Create the session
       const expires = new Date(Date.now() + 60 * 60 * 24 * 1000);
       const session = await encrypt({ user, expires });
-      // Save the session in a cookie
-      cookies().set("session", session, { expires, httpOnly: true });
-      if (cookieStore.get("cart")) {
-        const cart = cookieStore.get("cart")?.value;
-        createSanityUser(user, cart);
-        if(userName) cookieStore.set("user", userName);
-      } else {
-        createSanityUser(user, "");
-        if(userName) cookieStore.set("user", userName)
-      }
+      cookieStore.set("session",session, {expires: expires});
+      cookieStore.set("user", userName);
+      cookieStore.set("cart", cart);
+      loginSanityUser(user, cart, userName);
     }
   }
 }
@@ -150,7 +122,7 @@ export async function signIn(formData: FormData) {
   const user = {
     email: formData.get("email")?.toString(),
     pass: formData.get("password")?.toString(),
-    username: formData.get("username")?.toString()
+    username: formData.get("username")?.toString(),
   };
   // Create the session
   const expires = new Date(Date.now() + 60 * 60 * 24 * 1000);
@@ -158,11 +130,15 @@ export async function signIn(formData: FormData) {
   // Save the session in a cookie
   cookieStore.set("session", session, { expires, httpOnly: true });
   createSanityUser(user, "[]");
-  if(user.username) cookieStore.set("user", user.username)
+  if (user.username) cookieStore.set("user", user.username);
 }
 
 async function createSanityUser(
-  user: { email: string | undefined; pass: string | undefined, username:string|undefined },
+  user: {
+    email: string | undefined;
+    pass: string | undefined;
+    username: string | undefined;
+  },
   cart: string | undefined
 ) {
   revalidatePath("/");
@@ -172,7 +148,32 @@ async function createSanityUser(
     email: user.email,
     cart: cart,
     pass: await encrypt({ user }),
-    username: user.username
+    username: user.username,
+  };
+
+  try {
+    console.log(`Item imported successfully`);
+    await client.createOrReplace(doc);
+  } catch (error: any) {
+    console.error(`Error importing item :`, error.message);
+  }
+}
+async function loginSanityUser(
+  user: {
+    email: string | undefined;
+    pass: string | undefined;
+  },
+  cart: string | undefined,
+  userName:string|null
+) {
+  revalidatePath("/");
+  const doc: any = {
+    _type: "users",
+    _id: `${user.email?.split("@")[0]}`,
+    email: user.email,
+    cart: cart,
+    pass: await encrypt({ user }),
+    username: userName,
   };
 
   try {
@@ -184,8 +185,9 @@ async function createSanityUser(
 }
 
 export async function updateSanityUser(
-  user: { email: string | undefined; pass: string | undefined},
-  cart: string | undefined,username:string|undefined
+  user: { email: string | undefined; pass: string | undefined },
+  cart: string | undefined,
+  username: string | undefined
 ) {
   revalidatePath("/");
   const doc: any = {
@@ -194,7 +196,7 @@ export async function updateSanityUser(
     email: user.email,
     cart: cart,
     pass: await encrypt({ user }),
-    username: username
+    username: username,
   };
 
   try {
@@ -205,15 +207,14 @@ export async function updateSanityUser(
   }
 }
 
-export async function setSingleProductCookie(product:product) {
+export async function setSingleProductCookie(product: product) {
   cookies().set("produit", JSON.stringify(product));
 }
 
-export async function addToCart(searchParams:any, data:product) {
-  'use server';
+export async function addToCart(searchParams: any, data: product) {
+  "use server";
   let arr = [];
   let cart: string | undefined = cookies().get("cart")?.value;
-  const userName = cookies().get('user')?.value;
   if (cart && searchParams.color) {
     let bool = true;
     arr = JSON.parse(cart);
@@ -238,12 +239,22 @@ export async function addToCart(searchParams:any, data:product) {
         quantity: 1,
       });
     }
-    cookies().set({name: "cart", value: JSON.stringify(arr), path: '/', httpOnly: true});
+    cookies().set({
+      name: "cart",
+      value: JSON.stringify(arr),
+      path: "/",
+      httpOnly: true,
+    });
     if (cookies().get("session")) {
-      await updateSanityUser(await decryptForSanity(cookies().get("session")?.value), JSON.stringify(arr), userName);
+      const userName = cookies().get("user")?.value;
+      await updateSanityUser(
+        await decryptForSanity(cookies().get("session")?.value),
+        JSON.stringify(arr),
+        userName
+      );
     }
   } else {
-    if(searchParams.color) {
+    if (searchParams.color) {
       arr.push({
         id: data._id,
         color: searchParams.color,
@@ -252,11 +263,21 @@ export async function addToCart(searchParams:any, data:product) {
         material: searchParams.material,
         quantity: 1,
       });
-      cookies().set({name: "cart", value: JSON.stringify(arr), path: '/', httpOnly:true});
+      cookies().set({
+        name: "cart",
+        value: JSON.stringify(arr),
+        path: "/",
+        httpOnly: true,
+      });
       if (cookies().get("session")) {
-        await updateSanityUser(await decryptForSanity(cookies().get("session")?.value), JSON.stringify(arr), userName);
+        const userName = cookies().get("user")?.value;
+        await updateSanityUser(
+          await decryptForSanity(cookies().get("session")?.value),
+          JSON.stringify(arr),
+          userName
+        );
       }
     }
   }
-  revalidatePath('/');
+  revalidatePath("/");
 }
